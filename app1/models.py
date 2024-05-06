919fa4ed786bc272e6ed931a0997e02a0b50ddff
Eugene600 <ugowach160@gmail.com>
2024-05-06 20:59:39 +0300

    Made changes to model by adding Slug Probably going to delete the added migrations

--- app1/models.py
+++ app1/models.py
@@ -1,14 +1,20 @@
 from django.db import models
+from django.utils.text import slugify
 
 # Create your models here.
 class Article(models.Model):
     title = models.CharField(max_length=70)
     body = models.TextField()
     date_published = models.DateField(auto_now_add=True)
+    slug = models.SlugField(unique=True)  # Add a slug field
+
+    def save(self, *args, **kwargs):
+        # Generate the slug from the title
+        self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.title
         
     def snippet(self):
-        return self.body[:50] + "...."    
-    
+        return self.body[:50] + "...."
