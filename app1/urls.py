2afb6ca32c816500ed5610f165387e14f5997e9d
Eugene600 <ugowach160@gmail.com>
2024-05-06 21:50:00 +0300

    Used named Url's
    I am getting some errors though, but I will work on them.

--- app1/urls.py
+++ app1/urls.py
@@ -1,6 +1,8 @@
 from . import views
 from django.urls import path
 
+
 urlpatterns = [
-   path('articles/', views.articles), 
+   path('articles/', views.articles, name="list"), 
+   path('<slug:slug>/', views.article_detail, name="article_detail")
 ]
--- app1/views.py
+++ app1/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from .models import Article
+from django.http import HttpResponse
 
 # Create your views here.
 def articles(request):
     article = Article.objects.all().order_by('date_published')
     return render(request, 'articles/articles.html', {'article': article})
+
+def article_detail(request, slug):
+    return HttpResponse(slug)
