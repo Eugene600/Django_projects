57fe68b66291311498257249740a78a90830b5b3
Eugene600 <ugowach160@gmail.com>
2024-05-08 20:44:52 +0300

    corrected named url problem in articles.html file

--- app1/urls.py
+++ app1/urls.py
@@ -4,5 +4,5 @@ from django.urls import path
 
 urlpatterns = [
    path('articles/', views.articles, name="list"), 
-   path('<slug:slug>/', views.article_detail, name="article_detail")
+   path('articles/<slug:slug>/', views.article_detail, name='article_detail'),
 ]
