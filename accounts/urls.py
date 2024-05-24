4f39d32e21f0260ddc1b9bafdbba5ca1e8660c63
Eugene600 <ugowach160@gmail.com>
2024-05-24 12:13:14 +0300

    Added a menu bar and changed background image

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,5 +6,6 @@ app_name = 'accounts'
 urlpatterns = [
     path('signup/', views.sign_up, name="signup"),
     path('login/', views.log_in, name="login"),
+    path('logout/', views.log_out, name="logout"),
     
 ]
